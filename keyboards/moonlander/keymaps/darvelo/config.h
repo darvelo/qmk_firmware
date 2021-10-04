5e927958da3dfc50f4d957c525a9385b1a3e9000
David Arvelo <david@davidarvelo.com>
2021-10-04 13:47:16 -0400

    Add initial darvelo config

---
+++ keyboards/moonlander/keymaps/darvelo/config.h
@@ -0,0 +1,22 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"bn6vg/nzAq0"
+#define RGB_MATRIX_STARTUP_SPD 60
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(FF_PRELUDE)
+#define DEFAULT_LAYER_SONGS { \
+        SONG(SONIC_RING),   \
+        SONG(ONE_UP_SOUND),  \
+        SONG(MARIO_THEME), \
+        SONG(ZELDA_PUZZLE), \
+        }
+#endif
+
+#define UNICODE_SELECTED_MODES UC_MAC, UC_LNX
+/* #define UNICODE_SONG_MAC  SONG(SONIC_RING) */
+/* #define UNICODE_SONG_LNX  SONG(ONE_UP_SOUND) */
