f8ef479c59c449594c90deef3cb1fdb0e8d4de02
David Arvelo <david@davidarvelo.com>
2021-10-05 20:48:29 -0400

    Change caps lock (ESC) to Left Shift

--- keyboards/moonlander/keymaps/darvelo/keymap.c
+++ keyboards/moonlander/keymaps/darvelo/keymap.c
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC] = LAYOUT_moonlander(
         _______,          KC_EXLM,      KC_AT,            KC_HASH,      KC_DLR,           KC_PERC, UC_MOD,                  LT(MDIA, KC_SCLN), KC_CIRC, KC_AMPR,         KC_ASTR,     KC_LPRN, KC_RPRN,     _______,
         _______,          KC_Q,         KC_W,             KC_E,         KC_R,             KC_T,    _______,                 TG(GAME),          KC_Y,    TD(CT_LSND),     TD(CT_RSND), KC_O,    KC_P,        KC_BSLS,
-        LSFT_T(KC_ESC),   KC_A,         KC_S,             KC_D,         KC_F,             KC_G,    KC_HYPR,                 KC_MEH,            KC_H,    KC_J,            KC_K,        KC_L,    TD(CT_CPNC), KC_EQL,
+        KC_LSFT,          KC_A,         KC_S,             KC_D,         KC_F,             KC_G,    KC_HYPR,                 KC_MEH,            KC_H,    KC_J,            KC_K,        KC_L,    TD(CT_CPNC), KC_EQL,
         LT(SYMB, KC_GRV), KC_Z,         KC_X,             KC_C,         KC_V,             KC_B,                                                KC_N,    KC_M,            KC_COMM,     KC_DOT,  KC_SLSH,     KC_MINS,
         _______,          _______,      _______,          KC_LALT,      LGUI_T(KC_APP),            KC_APP,                  KC_LCTRL,                   LGUI_T(KC_APP),  _______,     KC_LBRC, KC_RBRC,     MO(SYMB),
                                                                         KC_SPC,           KC_BSPC, LCTL_T(KC_DEL),          LALT_T(KC_TAB),    KC_ESC,  KC_ENT
