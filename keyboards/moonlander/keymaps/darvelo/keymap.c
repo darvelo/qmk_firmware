37d7e5b2dc5c7809bdf3176cf143841e8c000741
David Arvelo <david@davidarvelo.com>
2021-10-05 20:32:32 -0400

    Move punctuation closer to home row

--- keyboards/moonlander/keymaps/darvelo/keymap.c
+++ keyboards/moonlander/keymaps/darvelo/keymap.c
@@ -129,12 +129,12 @@ void rgb_matrix_indicators_user(void) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC] = LAYOUT_moonlander(
-        KC_EQL,           KC_EXLM,      KC_AT,            KC_HASH,      KC_DLR,           KC_PERC, UC_MOD,                  _______,        KC_CIRC, KC_AMPR,         KC_ASTR,     KC_LPRN, KC_RPRN,     KC_MINS,
-        _______,          KC_Q,         KC_W,             KC_E,         KC_R,             KC_T,    _______,                 TG(GAME),       KC_Y,    TD(CT_LSND),     TD(CT_RSND), KC_O,    KC_P,        KC_BSLS,
-        LSFT_T(KC_ESC),   KC_A,         KC_S,             KC_D,         KC_F,             KC_G,    KC_HYPR,                 KC_MEH,         KC_H,    KC_J,            KC_K,        KC_L,    TD(CT_CPNC), LT(MDIA, KC_SCLN),
-        _______,          KC_Z,         KC_X,             KC_C,         KC_V,             KC_B,                                             KC_N,    KC_M,            KC_COMM,     KC_DOT,  KC_SLSH,     _______,
-        LT(SYMB, KC_GRV), _______,      _______,          KC_LALT,      LGUI_T(KC_APP),            KC_APP,                  KC_LCTRL,                LGUI_T(KC_APP),  _______,     KC_LBRC, KC_RBRC,     MO(SYMB),
-                                                                        KC_SPC,           KC_BSPC, LCTL_T(KC_DEL),          LALT_T(KC_TAB), KC_ESC,  KC_ENT
+        _______,          KC_EXLM,      KC_AT,            KC_HASH,      KC_DLR,           KC_PERC, UC_MOD,                  LT(MDIA, KC_SCLN), KC_CIRC, KC_AMPR,         KC_ASTR,     KC_LPRN, KC_RPRN,     _______,
+        _______,          KC_Q,         KC_W,             KC_E,         KC_R,             KC_T,    _______,                 TG(GAME),          KC_Y,    TD(CT_LSND),     TD(CT_RSND), KC_O,    KC_P,        KC_BSLS,
+        LSFT_T(KC_ESC),   KC_A,         KC_S,             KC_D,         KC_F,             KC_G,    KC_HYPR,                 KC_MEH,            KC_H,    KC_J,            KC_K,        KC_L,    TD(CT_CPNC), KC_EQL,
+        LT(SYMB, KC_GRV), KC_Z,         KC_X,             KC_C,         KC_V,             KC_B,                                                KC_N,    KC_M,            KC_COMM,     KC_DOT,  KC_SLSH,     KC_MINS,
+        _______,          _______,      _______,          KC_LALT,      LGUI_T(KC_APP),            KC_APP,                  KC_LCTRL,                   LGUI_T(KC_APP),  _______,     KC_LBRC, KC_RBRC,     MO(SYMB),
+                                                                        KC_SPC,           KC_BSPC, LCTL_T(KC_DEL),          LALT_T(KC_TAB),    KC_ESC,  KC_ENT
     ),
 
     [LINUX] = LAYOUT_moonlander(
