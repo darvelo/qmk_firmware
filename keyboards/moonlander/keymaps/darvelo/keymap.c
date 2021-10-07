dfca04cf4a0a54910a5160eb883b7d252c35fa2a
David Arvelo <david@davidarvelo.com>
2021-10-06 22:51:20 -0400

    Remove redundant KC_SLSH for now

--- keyboards/moonlander/keymaps/darvelo/keymap.c
+++ keyboards/moonlander/keymaps/darvelo/keymap.c
@@ -265,7 +265,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(VIM),          KC_EXLM,      KC_AT,            KC_HASH,            KC_DLR,             KC_PERC, KC_MAC_TABBCK,           KC_MAC_TABFWD,     KC_CIRC, KC_AMPR,            KC_ASTR,     KC_LPRN, KC_RPRN,     UC_MOD,
         _______,          KC_Q,         KC_W,             KC_E,               KC_R,               KC_T,    KC_APP,                  _______,           KC_Y,    TD(CT_LSND),        TD(CT_RSND), KC_O,    KC_P,        KC_BSLS,
         KC_LSFT,          KC_A,         KC_S,             KC_D,               KC_F,               KC_G,    KC_HYPR,                 KC_MEH,            KC_H,    KC_J,               KC_K,        KC_L,    TD(CT_CPNC), TD(CT_EPNC),
-        LT(SYMB, KC_GRV), KC_Z,         KC_X,             KC_C,               KC_V,               KC_B,                                                KC_N,    KC_M,               KC_COMM,     KC_DOT,  KC_SLSH,     KC_MINS,
+        LT(SYMB, KC_GRV), KC_Z,         KC_X,             KC_C,               KC_V,               KC_B,                                                KC_N,    KC_M,               KC_COMM,     KC_DOT,  _______,     KC_MINS,
         _______,          _______,      _______,          LM(NUMS, MOD_LALT), LM(NUMS, MOD_LGUI),          TG(GAME),                LT(MDIA, KC_SCLN),          LM(NUMS, MOD_LGUI), _______,     KC_LBRC, KC_RBRC,     MO(SYMB),
                                                                               KC_SPC,             KC_BSPC, LCTL_T(KC_DEL),          LALT_T(KC_TAB),    KC_ESC,  KC_ENT
     ),
