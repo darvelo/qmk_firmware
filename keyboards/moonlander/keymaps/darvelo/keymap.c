c365715b096c37c732b536e8cc5015bf871ee73e
David Arvelo <david@davidarvelo.com>
2022-12-22 22:06:09 +0700

    Move the leader key

--- keyboards/moonlander/keymaps/darvelo/keymap.c
+++ keyboards/moonlander/keymaps/darvelo/keymap.c
@@ -505,12 +505,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // MEH  = ctrl + shift + alt
     // HYPR = ctrl + shift + alt + gui
     [MAC] = LAYOUT_moonlander(
-        _______,        KC_1,     KC_2,      KC_3,      KC_4,    KC_5,    KC_MAC_TABBCK,         KC_MAC_TABFWD, KC_6,             KC_7,          KC_8,    KC_9,    KC_0,    KC_MINS,
-        _______,        KC_Q,     KC_W,      KC_E,      KC_R,    KC_T,    _______,               _______,       KC_Y,             KC_U,          KC_I,    KC_O,    KC_P,    KC_EQL,
-        LCTL_T(KC_TAB), KC_A,     KC_S,      KC_D,      KC_F,    KC_G,    _______,               _______,       KC_H,             KC_J,          KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        _______,        KC_Z,     KC_X,      KC_C,      KC_V,    KC_B,                                          KC_N,             KC_M,          KC_COMM, KC_DOT,  KC_SLSH, KC_DQT,
-        KC_LEAD,        TT(MDIA), TO(BLNDR), KC_LALT,   KC_LGUI,          TT(NUMS),              TO(CODE),                        _______,       _______, _______, RGB_MOD, KC_APP,
-                                       LT(SYMB, KC_SPC),  LT(NUMS, KC_BSPC), LALT_T(KC_DEL),        TT(MVMT),      LT(MVMT, KC_ENT), LSFT_T(KC_ESC)
+        _______,        KC_1,     KC_2,      KC_3,      KC_4,              KC_5,              KC_MAC_TABBCK,           KC_MAC_TABFWD, KC_6,             KC_7,          KC_8,    KC_9,    KC_0,    KC_MINS,
+        _______,        KC_Q,     KC_W,      KC_E,      KC_R,              KC_T,              _______,                 _______,       KC_Y,             KC_U,          KC_I,    KC_O,    KC_P,    KC_EQL,
+        LCTL_T(KC_TAB), KC_A,     KC_S,      KC_D,      KC_F,              KC_G,              _______,                 _______,       KC_H,             KC_J,          KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        _______,        KC_Z,     KC_X,      KC_C,      KC_V,              KC_B,                                                      KC_N,             KC_M,          KC_COMM, KC_DOT,  KC_SLSH, KC_DQT,
+        _______,        TT(MDIA), TO(BLNDR), KC_LALT,   KC_LGUI,                              _______,                 TO(CODE),                        _______,       _______, _______, RGB_MOD, KC_APP,
+                                                        LT(SYMB, KC_SPC),  LT(NUMS, KC_BSPC), LALT_T(KC_DEL),          KC_LEAD,       LT(MVMT, KC_ENT), LSFT_T(KC_ESC)
     ),
     // I don't explicitly switch to this layer. I use the UC_MOD to programmatically swap the base layer between the MAC and LINUX layers.
     [LINUX] = LAYOUT_moonlander(
